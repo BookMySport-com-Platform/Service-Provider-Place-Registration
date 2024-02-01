67925188b3b4d987380d319929f256908078e867
VortexProjects <vortex1649@gmail.com>
2024-02-01 12:23:48 +0530

    Feat: Add the feature of number of courts and price per hour per court

--- src/main/java/com/bookmysport/service_provider_place_reg/Controllers/SPMainController.java
+++ src/main/java/com/bookmysport/service_provider_place_reg/Controllers/SPMainController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookmysport.service_provider_place_reg.MiddleWares.GetSPDetailsMW;
 import com.bookmysport.service_provider_place_reg.Models.ImagesDB;
+import com.bookmysport.service_provider_place_reg.Models.PriceSportCourts;
 import com.bookmysport.service_provider_place_reg.Models.ResponseMessage;
 import com.bookmysport.service_provider_place_reg.Models.SportsDB;
 import com.bookmysport.service_provider_place_reg.Services.FetchSportsImages;
@@ -19,7 +21,7 @@ import com.bookmysport.service_provider_place_reg.Services.ImageUploadService;
 import com.bookmysport.service_provider_place_reg.Services.UploadSportsService;
 
 @RestController
-@RequestMapping("spr/api")
+@RequestMapping("api")
 public class SPMainController {
 
     @Autowired
@@ -41,8 +43,8 @@ public class SPMainController {
 
     @PostMapping("uploadsports")
     public ResponseEntity<ResponseMessage> uploadSports(@RequestHeader String token, @RequestHeader String role,
-            @RequestHeader List<String> sports) {
-        return uploadSportsService.uploadSportsInfoService(sports, token, role);
+            @RequestBody List<PriceSportCourts> pricePerSport) {
+        return uploadSportsService.uploadSportsInfoService(pricePerSport, token, role);
     }
 
     @PostMapping("uploadimages")
--- src/main/java/com/bookmysport/service_provider_place_reg/Services/UploadSportsService.java
+++ src/main/java/com/bookmysport/service_provider_place_reg/Services/UploadSportsService.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.bookmysport.service_provider_place_reg.MiddleWares.GetSPDetailsMW;
+import com.bookmysport.service_provider_place_reg.Models.PriceSportCourts;
 import com.bookmysport.service_provider_place_reg.Models.ResponseMessage;
 import com.bookmysport.service_provider_place_reg.Models.SportsDB;
 import com.bookmysport.service_provider_place_reg.Repositories.SportsDBRepo;
@@ -25,20 +26,42 @@ public class UploadSportsService {
     @Autowired
     private SportsDBRepo sportsDBRepo;
 
-    public ResponseEntity<ResponseMessage> uploadSportsInfoService(List<String> sports, String token, String role) {
+    public ResponseEntity<ResponseMessage> uploadSportsInfoService(List<PriceSportCourts> pricePerSport, String token,
+            String role) {
         try {
             ResponseEntity<ResponseMessage> spId = getSPDetailsMW.getSPDetailsByToken(token, role);
 
-            for (int i = 0; i < sports.size(); i++) {
-                SportsDB sportsdb=new SportsDB();
-                sportsdb.setSpId(UUID.fromString(spId.getBody().getMessage()));
-                sportsdb.setSportName(sports.get(i));
-                sportsDBRepo.save(sportsdb);
+            String duplicatesMessage = "";
+
+            for (int i = 0; i < pricePerSport.size(); i++) {
+
+                SportsDB sportExistence=sportsDBRepo.findBySportName(pricePerSport.get(i).getSport());
+
+                if (sportExistence == null) {
+                    SportsDB sportsdb = new SportsDB();
+                    sportsdb.setSpId(UUID.fromString(spId.getBody().getMessage()));
+                    sportsdb.setSportName(pricePerSport.get(i).getSport());
+                    sportsdb.setPricePerHour(pricePerSport.get(i).getPrice());
+                    sportsdb.setNumberOfCourts(pricePerSport.get(i).getNumberOfCourts());
+                    sportsDBRepo.save(sportsdb);
+                } else {
+                    duplicatesMessage += sportsDBRepo.findBySportName(pricePerSport.get(i).getSport()).getSportName();
+                    duplicatesMessage += ", ";
+                }
+
             }
 
-            responseMessage.setSuccess(true);
-            responseMessage.setMessage("Sports registered successfully");
-            return ResponseEntity.ok().body(responseMessage);
+            if (duplicatesMessage.length() == 0) {
+                responseMessage.setSuccess(true);
+                responseMessage.setMessage("Sports registered successfully");
+                return ResponseEntity.ok().body(responseMessage);
+            } else {
+                responseMessage.setSuccess(false);
+                responseMessage.setMessage("Sports registered successfully but these " + duplicatesMessage
+                        + " already exists, hence not been added.");
+                return ResponseEntity.ok().body(responseMessage);
+            }
+
         } catch (Exception e) {
             responseMessage.setSuccess(false);
             responseMessage.setMessage(e.getMessage());
